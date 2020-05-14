5e6a678a427876bc6a572feebb51b8b3d4ced1bd
supertsp <tiago.souza.pedroso@gmail.com>
2020-05-14 15:19:32 -0300

    Projeto inicial

---
+++ app/src/main/java/com/example/capivarawars/ActivityLoading.kt
@@ -0,0 +1,12 @@
+package com.example.capivarawars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityLoading : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
---
+++ app/src/main/java/com/example/capivarawars/ActivityRegistrationPass02.kt
@@ -0,0 +1,23 @@
+package com.example.capivarawars
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class ActivityRegistrationPass02 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_pass02)
+    }
+
+    fun onClick_backToPass01(v: View) {
+        super.onBackPressed()
+    }
+
+    fun onClick_nextToPass03(v: View) {
+        val activityRegistrationPass03 = Intent(this, ActivityRegistrationPass03::class.java)
+        startActivity(activityRegistrationPass03)
+    }
+}
